ce9df3d743101919cfeaeaf19940657ff2446248
zhen yong chen <dannychen111@yahoo.com>
2019-12-17 15:24:20 -0600

    models are made and some orm.js is changed

--- config/orm.js
+++ config/orm.js
@@ -1,25 +1,25 @@
 var connection = require("./connection");
 
 var orm = {
-    selectAll: function () {
+    selectAll: function (cb) {
         var query = "SELECT * FROM burgers";
         connection.query(query, function (err, result) {
             if (err) throw err;
-            return result;
+            cb(result);
         });
     },
-    insertOne: function (name, devoured) {
-        var query = "INSERT INTO burgers_db.burgers (burger_name, devoured) VALUE ( ? , ?)";
+    insertOne: function (name, devoured, cb) {
+        var query = "INSERT INTO burgers (burger_name, devoured) VALUE ( ? , ?)";
         connection.query(query, [name, devoured] ,function (err, result) {
             if (err) throw err;
-            return result;
+            cb(result);
         });
     },
-    updateOne: function(column, newValue, id ){
+    updateOne: function(column, newValue, id, cb ){
         var query = "UPDATE burgers SET ?? = ? WHERE id = ?";
         connection.query(query,[column, newValue, id], function (err, result) {
             if (err) throw err;
-            return result;
+            cb(result);
         });
     }
 };
