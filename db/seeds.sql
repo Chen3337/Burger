6c1441101a693c3557dd39602c546d10ca5eec38
zhen yong chen <dannychen111@yahoo.com>
2019-12-17 00:00:10 -0600

    db schema and seed is completed

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers_db.burgers (burger_name, devoured) VALUE ("cheese burger", false);
+INSERT INTO burgers_db.burgers (burger_name, devoured) VALUE ("chicken burger", false);
+INSERT INTO burgers_db.burgers (burger_name, devoured) VALUE ("beef burger", true);
